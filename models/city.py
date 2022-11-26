64539ae4ee2b2e60f0ef0832bfc93c1b73f6cb80
Timothy Kamau <kamautimothy635@gmail.com>
2022-11-26 08:43:24 +0000

    AirBnB_clone

--- models/city.py
+++ models/city.py
@@ -4,30 +4,15 @@ Defines the City class
 """
 from models.base_model import BaseModel
 
-<<<<<<< HEAD
-from models.base_model import BaseModel
-=======
->>>>>>> 1301391c040ad1ac86f8027bbd91c765f62248a1
 
-class City(BaseModel):
-    """Represent a city
-    Attributes:
-        state_id (str): The state id.
-        name (str): The name of the city
-    """
-
-<<<<<<< HEAD
 class City(BaseModel):
         """Represent a city
-            
+        
         Attributes:
-            state_id (str): The state id.
-            name (str): The name of the city
+                state_id (str): The state id.
+                name (str): The name of the city
+        
         """
         
         state_id = ""
         name = ""
-=======
-    state_id = ""
-    name = ""
->>>>>>> 1301391c040ad1ac86f8027bbd91c765f62248a1
